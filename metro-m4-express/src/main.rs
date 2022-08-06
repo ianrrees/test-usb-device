1f2daa3184ff31077965e9f72a936375da927021
Ian Rees <code@ianrees.nz>
2022-08-06 13:10:18 +1200

    Adding USB support
    The device doesn't enumerate.

--- metro-m4-express/src/main.rs
+++ metro-m4-express/src/main.rs
@@ -3,24 +3,25 @@
 
 use metro_m4 as bsp;
 
-use bsp::ehal;
 use bsp::hal;
 use bsp::pac;
 
 use panic_halt as _;
 
 use bsp::entry;
+use cortex_m::peripheral::NVIC;
 use hal::clock::GenericClockController;
 use hal::prelude::*;
-use pac::{CorePeripherals, Peripherals};
+use hal::usb::UsbBus;
+use pac::{interrupt, CorePeripherals, Peripherals};
 
-use ehal::blocking::delay::DelayMs;
-use hal::delay::Delay;
+use usb_device::bus::UsbBusAllocator;
+use usb_device::prelude::*;
 
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
-    let core = CorePeripherals::take().unwrap();
+    let mut core = CorePeripherals::take().unwrap();
     let mut clocks = GenericClockController::with_external_32kosc(
         peripherals.GCLK,
         &mut peripherals.MCLK,
@@ -30,11 +31,78 @@ fn main() -> ! {
     );
     let pins = bsp::Pins::new(peripherals.PORT);
     let mut red_led = pins.d13.into_push_pull_output();
-    let mut delay = Delay::new(core.SYST, &mut clocks);
-    loop {
-        delay.delay_ms(1000u32);
-        red_led.set_high().unwrap();
-        delay.delay_ms(1000u32);
-        red_led.set_low().unwrap();
+
+    red_led.set_high().unwrap(); // LED on
+
+    let bus_allocator = unsafe {
+        USB_ALLOCATOR = Some(bsp::usb_allocator(
+            peripherals.USB,
+            &mut clocks,
+            &mut peripherals.MCLK,
+            pins.usb_dm,
+            pins.usb_dp,
+        ));
+        USB_ALLOCATOR.as_ref().unwrap()
+    };
+
+    unsafe {
+        // USB_SERIAL = Some(SerialPort::new(bus_allocator));
+        USB_BUS = Some(
+            UsbDeviceBuilder::new(bus_allocator, UsbVidPid(0x2222, 0x3333))
+                .manufacturer("Fake company")
+                .product("Serial port")
+                .serial_number("TEST")
+                // .device_class(USB_CLASS_CDC)
+                .build(),
+        );
     }
+
+    unsafe {
+        core.NVIC.set_priority(interrupt::USB_TRCPT0, 1);
+        NVIC::unmask(interrupt::USB_TRCPT0);
+        core.NVIC.set_priority(interrupt::USB_TRCPT1, 1);
+        NVIC::unmask(interrupt::USB_TRCPT1);
+        core.NVIC.set_priority(interrupt::USB_SOF_HSOF, 1);
+        NVIC::unmask(interrupt::USB_SOF_HSOF);
+        core.NVIC.set_priority(interrupt::USB_OTHER, 1);
+        NVIC::unmask(interrupt::USB_OTHER);
+    }
+
+    red_led.set_low().unwrap(); // LED off
+
+    loop {}
+}
+
+static mut USB_ALLOCATOR: Option<UsbBusAllocator<UsbBus>> = None;
+static mut USB_BUS: Option<UsbDevice<UsbBus>> = None;
+// static mut USB_SERIAL: Option<SerialPort<UsbBus>> = None;
+
+fn poll_usb() {
+    unsafe {
+        if let Some(usb_dev) = USB_BUS.as_mut() {
+            // if let Some(serial) = USB_SERIAL.as_mut() {
+                usb_dev.poll(&mut []);
+            // };
+        }
+    };
+}
+
+#[interrupt]
+fn USB_TRCPT0() {
+    poll_usb();
+}
+
+#[interrupt]
+fn USB_TRCPT1() {
+    poll_usb();
+}
+
+#[interrupt]
+fn USB_SOF_HSOF() {
+    poll_usb();
+}
+
+#[interrupt]
+fn USB_OTHER() {
+    poll_usb();
 }
